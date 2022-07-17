a38a3d29c955a95ab7fa7dcd740ca7e2b1b73c66
WilliamKelly00 <williammkelly00@gmail.com>
2022-07-16 22:14:27 -0400

    first commit

---
+++ src/Lox.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lox {
+
+    static boolean hasError = false;
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+            "[line " + line + "] Error" + where + ": " + message);
+        hasError = true;
+    }
+
+    public static void run(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+        
+        
+        for(Token token : tokens){
+            System.out.println(token);
+        }
+    }
+    
+    public static void runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.print("> ");
+            String line = reader.readLine();
+            if(line == null) break;
+            run(line);
+            // dont kill session if there is an error
+            hasError = false;
+        }
+    }
+    
+    public static void runFile(String path) throws IOException{
+        byte[] bytes = Files.readAllBytes(Path.of(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        // error in source code
+        if(hasError) System.exit(65);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        if(args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if(args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+} 
