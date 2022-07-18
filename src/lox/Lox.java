894ea2a0723019ee7b2c41af3fa33074d7f76443
WilliamKelly00 <williammkelly00@gmail.com>
2022-07-18 18:36:02 -0400

    added parser

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -15,6 +15,14 @@ public class Lox {
         report(line, "", message);
     }
 
+    static void error(Token token, String message){
+        if(token.type == TokenType.EOF){
+            report(token.line, " at end", message);
+        }else{
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
+
     private static void report(int line, String where, String message) {
         System.err.println(
             "[line " + line + "] Error" + where + ": " + message);
@@ -24,11 +32,14 @@ public class Lox {
     public static void run(String source){
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-        
-        
-        for(Token token : tokens){
-            System.out.println(token);
+        Parser parser = new Parser(tokens);
+        Expr expr = parser.parse();
+
+        if(hadError){
+            return;
         }
+        
+        System.out.println(new AstPrinter().print(expr));
     }
     
     public static void runPrompt() throws IOException{
