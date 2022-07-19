a05ecb62ee2531bed6dcb0ffeee31b12dd205322
WilliamKelly00 <williammkelly00@gmail.com>
2022-07-19 13:48:01 -0400

    added scope, shadowing and variables

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -2,14 +2,19 @@ package lox;
 
 import java.util.List;
 
+import lox.Expr.Assign;
 import lox.Expr.Binary;
 import lox.Expr.Grouping;
 import lox.Expr.Literal;
 import lox.Expr.Unary;
+import lox.Expr.Variable;
 import lox.Stmt.Expression;
 import lox.Stmt.Print;
+import lox.Stmt.Var;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+    private Environment environment = new Environment();
 
     void interpret(List<Stmt> statements){
         try{
@@ -100,6 +105,25 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         stmt.accept(this);
     }
 
+    void executeBlock(List<Stmt> statements, Environment environment){
+        Environment previous = this.environment;
+        try{
+            this.environment = environment;
+
+            for(Stmt statement : statements){
+                execute(statement);
+            }
+        } finally {
+            this.environment = previous;
+        }
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt){
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+
     private boolean isTruthy(Object object){
         if(object == null) return false;
         if(object instanceof Boolean) return (boolean)object;
@@ -148,5 +172,28 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
         return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Var stmt) {
+        Object value = null;
+        if(stmt.initializer != null){
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Variable expr) {
+        return environment.get(expr.name);
+    }
+
+    @Override
+    public Object visitAssignExpr(Assign expr) {
+        Object value  = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
     }
 }
--- src/lox/Parser.java
+++ src/lox/Parser.java
@@ -16,14 +16,41 @@ public class Parser {
     List<Stmt> parse(){
         List<Stmt> statements = new ArrayList<>();
         while(!isAtEnd()){
-            statements.add(statement());
+            statements.add(declaration());
         }
 
         return statements;
     }
 
     private Expr expression(){
-        return equality();
+        return assignment();
+    }
+
+    private Expr assignment(){
+        Expr expr = equality();
+
+        if(match(TokenType.EQUAL)){
+            Token equals = previous();
+            Expr value = assignment();
+            
+            if(expr instanceof Expr.Variable){
+                Token name = ((Expr.Variable)expr).name;
+                return new Expr.Assign(name, value);
+            }
+
+            error(equals, "Invalid assignment target.");
+        }
+        return expr;
+    }
+
+    private Stmt declaration(){
+        try{
+            if(match(TokenType.VAR)) return varDeclaration();
+            return statement();
+        } catch (ParseError error){
+            synchronize();
+            return null;
+        }
     }
 
     private Stmt statement(){
@@ -36,6 +63,19 @@ public class Parser {
         Expr value = expression();
         consume(TokenType.SEMICOLON, "Expect ';' after value.");
         return new Stmt.Print(value);
+    }
+
+    private Stmt varDeclaration(){
+        Token name = consume(TokenType.IDENTIFIER, "Expect variable name.");
+
+        Expr initializer = null;
+
+        if(match(TokenType.EQUAL)){
+            initializer = expression();
+        }
+
+        consume(TokenType.SEMICOLON, "Expect ';' after variable declaration.");
+        return new Stmt.Var(name, initializer);
     }
 
     private Stmt expressionStatement(){
@@ -109,6 +149,10 @@ public class Parser {
 
         if(match(TokenType.NUMBER, TokenType.STRING)){
             return new Expr.Literal(previous().literal);
+        }
+
+        if(match(TokenType.IDENTIFIER)){
+            return new Expr.Variable(previous());
         }
 
         if(match(TokenType.LEFT_PAREN)){
